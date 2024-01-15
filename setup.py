1a711791b92bf2fe7826a906d84f0279b9e6db32
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-01-15 11:36:15 -0500

    Pin django<4.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
-        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 4.2',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
@@ -37,7 +37,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'celery',
-        'django',
+        'django<4.2',
         'django-allauth',
         'django-configurations[database,email]',
         'django-extensions',
