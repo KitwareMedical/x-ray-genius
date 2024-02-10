9fa6f7466ce8a45dc6dc4a31b13ee6da8ac7d55c
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-02-10 11:39:09 -0500

    Add tests for permission checking

--- xray_genius/core/tests/factories.py
+++ xray_genius/core/tests/factories.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 import factory.django
 
+from xray_genius.core.models import CTInputFile, InputParameters, Session
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -10,3 +12,33 @@ class UserFactory(factory.django.DjangoModelFactory):
     email = factory.Faker('safe_email')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
+
+
+class CTInputFileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CTInputFile
+
+    file = factory.django.FileField(filename='test_file.nrrd', data=b'fakefile')
+
+
+class SessionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Session
+
+    owner = factory.SubFactory(UserFactory)
+    parameters = factory.RelatedFactory(
+        factory='xray_genius.core.tests.factories.InputParametersFactory',
+        factory_related_name='session',
+    )
+    input_scan = factory.SubFactory(CTInputFileFactory)
+
+
+class InputParametersFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = InputParameters
+
+    session = factory.SubFactory(factory=SessionFactory)
+
+    carm_alpha = factory.Faker('pyint', min_value=0, max_value=360)
+    carm_beta = factory.Faker('pyint', min_value=0, max_value=360)
+    source_to_detector_distance = factory.Faker('pyfloat', min_value=0.0, max_value=500.0)
