1da40427c8e874d791fa72de822c93c870decbe1
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-02-10 11:38:47 -0500

    Add permission checking to all views

--- xray_genius/core/views.py
+++ xray_genius/core/views.py
@@ -1,7 +1,10 @@
+from collections.abc import Callable
+from typing import ParamSpec, TypeVar
 from urllib.parse import urlencode
 
 from django.db import transaction
 from django.http import HttpRequest, HttpResponseBadRequest
+from django.http.response import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.decorators.http import require_POST
@@ -10,7 +13,22 @@ from .forms import CTInputFileUploadForm
 from .models import Session
 from .tasks import run_deepdrr_task
 
+T = TypeVar('T')
+P = ParamSpec('P')
 
+
+def permission_check(view: Callable[P, T]) -> Callable[P, T]:
+    def check(request: HttpRequest, *args, **kwargs):
+        if session_pk := kwargs.get('session_pk'):
+            session = get_object_or_404(Session, pk=session_pk)
+            if session.owner != request.user:
+                raise Http404()
+        return view(request, *args, **kwargs)
+
+    return check
+
+
+@permission_check
 def dashboard(request: HttpRequest):
     sessions = Session.objects.filter(owner=request.user)
     return render(
@@ -23,6 +41,7 @@ def dashboard(request: HttpRequest):
     )
 
 
+@permission_check
 def upload_ct_input_file(request: HttpRequest):
     if request.method == 'POST':
         form = CTInputFileUploadForm(request.POST, request.FILES)
@@ -47,16 +66,19 @@ def upload_ct_input_file(request: HttpRequest):
     )
 
 
+@permission_check
 def download_ct_file(request: HttpRequest, session_pk: str):
     session = get_object_or_404(Session, pk=session_pk)
     return redirect(session.input_scan.file.url)
 
 
+@permission_check
 def volview_viewer(request: HttpRequest, session_pk: str):
     session = get_object_or_404(Session, pk=session_pk)
     return render(request, 'viewer.html', context={'session': session})
 
 
+@permission_check
 @require_POST
 def initiate_batch_run(request: HttpRequest, session_pk: str):
     with transaction.atomic():
