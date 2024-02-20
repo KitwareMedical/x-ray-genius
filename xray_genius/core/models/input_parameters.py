9f2b54ef315f3a307e3701a161a2abccb279d8bb
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-02-20 13:18:29 -0500

    Allow rotational angle params to be null

--- xray_genius/core/models/input_parameters.py
+++ xray_genius/core/models/input_parameters.py
@@ -7,10 +7,10 @@ class InputParameters(models.Model):
     session = models.OneToOneField(Session, related_name='parameters', on_delete=models.CASCADE)
 
     carm_alpha = models.IntegerField(
-        help_text='The desired alpha angulation of the C-arm in degrees.'
+        null=True, blank=True, help_text='The desired alpha angulation of the C-arm in degrees.'
     )
     carm_beta = models.IntegerField(
-        help_text='The desired secondary angulation of the C-arm in degrees.'
+        null=True, blank=True, help_text='The desired secondary angulation of the C-arm in degrees.'
     )
     source_to_detector_distance = models.FloatField(
         help_text='The distance from the source to the detector in mm.'
