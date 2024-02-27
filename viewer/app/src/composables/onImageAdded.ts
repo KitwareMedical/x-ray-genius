eb6dcd59568cd94774ded7976b19fe10c578b914
Forrest <forrest.li@kitware.com>
2024-02-27 17:18:20 -0500

    viewer: fix image re-centering

--- viewer/app/src/composables/onImageAdded.ts
+++ viewer/app/src/composables/onImageAdded.ts
@@ -1,13 +1,17 @@
 import { useImageStore } from '@/src/store/datasets-images';
 import { storeToRefs } from 'pinia';
-import { watch } from 'vue';
+import { watch, watchEffect } from 'vue';
 
 export function onImageAdded(callback: (added: string) => void) {
   const { dataIndex } = storeToRefs(useImageStore());
-  return watch(dataIndex, (newIndex, oldIndex) => {
-    const added = Object.keys(oldIndex).filter((id) => id in newIndex);
-    added.forEach((id) => {
-      callback(id);
-    });
-  });
+  return watch(
+    dataIndex,
+    (newIndex, oldIndex) => {
+      const added = Object.keys(oldIndex).filter((id) => id in newIndex);
+      added.forEach((id) => {
+        callback(id);
+      });
+    },
+    { deep: true }
+  );
 }
