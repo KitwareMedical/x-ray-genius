239424135688119a92e806b3288f4e6c27de55d4
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-03-07 10:17:28 -0500

    Add filename to parameters column

--- xray_genius/core/models/ct_input_file.py
+++ xray_genius/core/models/ct_input_file.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
 from django.db import models
 from s3_file_field.fields import S3FileField
 
 
 class CTInputFile(models.Model):
     file = S3FileField()
+
+    @property
+    def filename(self):
+        return Path(self.file.name).name
