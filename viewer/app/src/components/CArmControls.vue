4ddaf3212d2d25f7a3f3eea88af4f057b09c9e4a
Forrest <forrest.li@kitware.com>
2024-03-15 14:38:43 -0400

    viewer: group c-arm control UI elements

--- viewer/app/src/components/CArmControls.vue
+++ viewer/app/src/components/CArmControls.vue
@@ -87,132 +87,146 @@ async function submit() {
 </script>
 
 <template>
-  <div class="controls">
-    <c-arm-dial
-      v-model="dialPosition"
-      :size="200"
-      @start-drag="startDrag"
-      @end-drag="endDrag"
-      :disabled="store.randomizeRotation"
-    ></c-arm-dial>
-    <div>{{ (dialPosition * 100).toFixed(0) }}%</div>
-    <v-row>
-      <v-col cols="3">
-        <v-label class="mr-5"> Rotation <br />Concentration </v-label>
-      </v-col>
-      <v-col cols="3">
-        <v-text-field
-          v-model="rotationKappa"
-          outlined
-          type="number"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="6">
-        <v-checkbox v-model="store.randomizeRotation">
-          <template v-slot:label>
-            <span class="mr-2">Randomize rotation (alpha)</span>
-            <v-tooltip bottom>
-              <template v-slot:activator="{ props }">
-                <v-icon v-bind="props" class="help-icon">
-                  mdi-help-circle-outline
-                </v-icon>
-              </template>
-              <p>
-                Checking this box will randomize the rotation of the C-Arm on
-                each batch
-                <br />
-                generation instead of fixing it to a constant value.
-              </p>
-            </v-tooltip>
-          </template>
-        </v-checkbox>
-      </v-col>
-    </v-row>
-    <v-slider
-      v-model="xTranslation"
-      min="0"
-      max="1"
-      step="0.01"
-      style="width: 80%"
-      class="mt-5"
-      label="X Translation"
-      @pointerdown="startDrag"
-    ></v-slider>
-    <v-slider
-      v-model="yTranslation"
-      min="0"
-      max="1"
-      step="0.01"
-      style="width: 80%"
-      label="Y Translation"
-      @pointerdown="startDrag"
-    ></v-slider>
-    <v-slider
-      v-model="zTranslation"
-      min="0"
-      max="1"
-      step="0.01"
-      style="width: 80%"
-      label="Z Translation"
-      @pointerdown="startDrag"
-    ></v-slider>
-    <v-slider
-      v-model="tilt"
-      :disabled="store.randomizeTilt"
-      min="0"
-      max="1"
-      step="0.01"
-      style="width: 80%"
-      label="Tilt"
-      @pointerdown="startDrag"
-    ></v-slider>
-    <v-row>
-      <v-col cols="3">
-        <v-label class="mr-5"> Tilt <br />Concentration </v-label>
-      </v-col>
-      <v-col cols="3">
-        <v-text-field v-model="tiltKappa" outlined type="number"></v-text-field>
-      </v-col>
-      <v-col cols="6">
-        <v-checkbox v-model="store.randomizeTilt" label="Randomize tilt (beta)">
-          <template v-slot:label>
-            <span class="mr-2">Randomize tilt (beta)</span>
-            <v-tooltip bottom>
-              <template v-slot:activator="{ props }">
-                <v-icon v-bind="props" class="help-icon">
-                  mdi-help-circle-outline
-                </v-icon>
-              </template>
-              <p>
-                Checking this box will randomize the tilt of the C-Arm on each
-                batch
-                <br />
-                generation instead of fixing it to a constant value.
-              </p>
-            </v-tooltip>
-          </template>
-        </v-checkbox>
-      </v-col>
-    </v-row>
-    <v-alert v-if="submissionError" color="error" class="mb-3">
-      <div class="d-flex flex-row align-center">
-        <v-icon class="mr-2">mdi-alert</v-icon>
-        <span>Failed to submit session to the server</span>
-      </div>
-    </v-alert>
-    <v-row>
-      <v-col cols="6">
-        <v-label>
-          Number of
-          <br />
-          Samples
-        </v-label>
-      </v-col>
-      <v-col cols="6">
-        <v-text-field v-model="numberOfSamples" outlined></v-text-field>
-      </v-col>
-    </v-row>
-    <v-btn :loading="submissionLoading" @click="submit">Submit</v-btn>
+  <div class="controls mt-3">
+    <div class="w-100 px-3">
+      <span class="text-subtitle-2">Rotation &amp; Tilt</span>
+      <v-divider height="2px" />
+      <v-row>
+        <v-col cols="6">
+          <c-arm-dial
+            v-model="dialPosition"
+            :size="200"
+            @start-drag="startDrag"
+            @end-drag="endDrag"
+            :disabled="store.randomizeRotation"
+          ></c-arm-dial>
+          <div class="text-center label mt-n8">Rotation (Rainbow)</div>
+          <v-checkbox v-model="store.randomizeRotation" class="mt-3">
+            <template v-slot:label>
+              <span class="mr-2">Randomize Rotation (alpha)</span>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ props }">
+                  <v-icon v-bind="props" class="help-icon">
+                    mdi-help-circle-outline
+                  </v-icon>
+                </template>
+                <p>
+                  Checking this box will randomize the rotation of the C-Arm on
+                  each batch
+                  <br />
+                  generation instead of fixing it to a constant value.
+                </p>
+              </v-tooltip>
+            </template>
+          </v-checkbox>
+          <v-text-field
+            v-model="rotationKappa"
+            outlined
+            type="number"
+            label="Rotation Concentration"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="6">
+          <c-arm-dial
+            v-model="tilt"
+            :size="200"
+            @start-drag="startDrag"
+            @end-drag="endDrag"
+            :disabled="store.randomizeTilt"
+          ></c-arm-dial>
+          <div class="text-center label mt-n8">Tilt (Head/Foot)</div>
+          <v-checkbox v-model="store.randomizeTilt" class="mt-3">
+            <template v-slot:label>
+              <span class="mr-2">Randomize Tilt</span>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ props }">
+                  <v-icon v-bind="props" class="help-icon">
+                    mdi-help-circle-outline
+                  </v-icon>
+                </template>
+                <p>
+                  Checking this box will randomize the tilt of the C-Arm on each
+                  batch
+                  <br />
+                  generation instead of fixing it to a constant value.
+                </p>
+              </v-tooltip>
+            </template>
+          </v-checkbox>
+          <v-text-field
+            v-model="tiltKappa"
+            outlined
+            type="number"
+            label="Tilt Concentration"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </div>
+    <div class="w-100 px-3">
+      <span class="text-subtitle-2">Translation</span>
+      <v-divider height="2px" />
+      <v-slider
+        v-model="xTranslation"
+        min="0"
+        max="1"
+        step="0.01"
+        class="mt-5"
+        label="Left/Right"
+        hide-details
+        @pointerdown="startDrag"
+      >
+        <template #append>
+          <v-checkbox hide-details label="Randomize"></v-checkbox>
+        </template>
+      </v-slider>
+      <v-slider
+        v-model="yTranslation"
+        min="0"
+        max="1"
+        step="0.01"
+        label="Front/Back"
+        hide-details
+        @pointerdown="startDrag"
+      >
+        <template #append>
+          <v-checkbox hide-details label="Randomize"></v-checkbox>
+        </template>
+      </v-slider>
+      <v-slider
+        v-model="zTranslation"
+        min="0"
+        max="1"
+        step="0.01"
+        label="Head/Foot"
+        hide-details
+        @pointerdown="startDrag"
+      >
+        <template #append>
+          <v-checkbox hide-details label="Randomize"></v-checkbox>
+        </template>
+      </v-slider>
+    </div>
+    <div class="w-100 px-3 d-flex flex-column">
+      <v-divider class="pb-5" height="2px" />
+      <v-alert v-if="submissionError" color="error" class="mb-3">
+        <div class="d-flex flex-row align-center">
+          <v-icon class="mr-2">mdi-alert</v-icon>
+          <span>Failed to submit session to the server</span>
+        </div>
+      </v-alert>
+      <v-text-field
+        label="Number of Samples"
+        v-model="numberOfSamples"
+        outlined
+      ></v-text-field>
+      <v-btn
+        variant="tonal"
+        color="secondary"
+        :loading="submissionLoading"
+        @click="submit"
+        >Submit</v-btn
+      >
+    </div>
   </div>
 </template>
 
@@ -221,5 +235,9 @@ async function submit() {
   display: flex;
   flex-flow: column;
   align-items: center;
+}
+
+.label {
+  opacity: var(--v-medium-emphasis-opacity);
 }
 </style>
