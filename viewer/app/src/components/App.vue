912372c4f0dc061cb4d74286f857ce0407b743ff
Forrest <forrest.li@kitware.com>
2024-03-15 15:20:59 -0400

    viewer: remove control strip

---
+++ viewer/app/src/components/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import VolViewApp from '@/src/components/App.vue';
+import { useViewStore } from '@/src/store/views';
+import { Layouts, DefaultLayoutName } from '../config';
+
+const viewStore = useViewStore();
+viewStore.setLayout(Layouts[DefaultLayoutName]);
+</script>
+
+<template>
+  <VolViewApp />
+</template>
