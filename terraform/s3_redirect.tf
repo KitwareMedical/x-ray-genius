192b6940629cf3891fe541a837315c368bb6c68a
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-03-18 15:13:03 -0400

    Redirect <www.>xray-genius.com

---
+++ terraform/s3_redirect.tf
@@ -0,0 +1,22 @@
+locals {
+  buckets = [
+    data.aws_route53_zone.this.name,
+    "www.${data.aws_route53_zone.this.name}",
+  ]
+}
+
+resource "aws_s3_bucket" "redirect" {
+  count = length(local.buckets)
+
+  bucket = local.buckets[count.index]
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect" {
+  count = length(local.buckets)
+
+  bucket = aws_s3_bucket.redirect[count.index].id
+
+  redirect_all_requests_to {
+    host_name = local.buckets[count.index]
+  }
+}
