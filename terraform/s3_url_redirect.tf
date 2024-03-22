4870b645375487e1f3f613d842103f90edda3683
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-03-22 12:25:37 -0400

    S3 buckets to redirect root domain to app subdomain
    With this change, any requests to xray-genius.com or
    www.xray-genius.com will be automatically redirected to
    app.xray-genius.com.

---
+++ terraform/s3_url_redirect.tf
@@ -0,0 +1,42 @@
+# This file contains infrastructure code to create S3 buckets for URL redirection from
+# the root domain to the app subdomain. This allows users to go to xray-genius.com or
+# www.xray-genius.com and be redirected to app.xray-genius.com.
+
+locals {
+  buckets = [
+    data.aws_route53_zone.this.name,
+    "www.${data.aws_route53_zone.this.name}",
+  ]
+}
+
+resource "aws_s3_bucket" "redirect" {
+  count = length(local.buckets)
+
+  bucket = local.buckets[count.index]
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect" {
+  count = length(local.buckets)
+
+  bucket = aws_s3_bucket.redirect[count.index].id
+
+  redirect_all_requests_to {
+    host_name = module.django.fqdn
+    protocol  = "http"
+  }
+}
+
+resource "aws_route53_record" "redirect" {
+  count = length(local.buckets)
+
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = local.buckets[count.index]
+  type    = "A"
+
+  alias {
+    name = aws_s3_bucket_website_configuration.redirect[count.index].website_domain
+    # From https://docs.aws.amazon.com/general/latest/gr/s3.html#s3_website_region_endpoints
+    zone_id                = "Z3AQBSTGFYJSTF"
+    evaluate_target_health = false
+  }
+}
