ffa07baf88cadfa1a9bdf79abe3bf75f4451c650
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-04-10 16:55:40 -0400

    Add Sentry project for viewer

--- terraform/sentry.tf
+++ terraform/sentry.tf
@@ -31,3 +31,21 @@ data "sentry_key" "django" {
   organization = data.sentry_organization.this.id
   project      = sentry_project.django.id
 }
+
+resource "sentry_project" "viewer" {
+  organization = data.sentry_organization.this.id
+
+  teams = [sentry_team.this.id]
+  name  = "xray-genius-viewer"
+  slug  = "xray-genius-viewer"
+
+  platform = "javascript-vue"
+
+  default_key   = true
+  default_rules = true
+}
+
+data "sentry_key" "viewer" {
+  organization = data.sentry_organization.this.id
+  project      = sentry_project.viewer.id
+}
