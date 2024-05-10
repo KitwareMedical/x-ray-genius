7b605731036250fcd3f821ec2cdf71776bd6f267
Forrest <forrest.li@kitware.com>
2024-05-10 11:10:24 -0400

    viewer: detector model should be a very thin slab

--- viewer/app/src/composables/useDetectorModel.ts
+++ viewer/app/src/composables/useDetectorModel.ts
@@ -1,12 +1,14 @@
 import { View } from '@/src/core/vtk/types';
 import { MaybeRef, ref, watchEffect } from 'vue';
-import { useGeometryActor, useGeometryActor2D } from './useActor';
+import { useGeometryActor2D } from './useActor';
 import { useCArmPosition } from './useCArmModel';
 import { Maybe } from '@/src/types';
 import { Vector3 } from '@kitware/vtk.js/types';
 import vtkCubeSource from '@kitware/vtk.js/Filters/Sources/CubeSource';
 import { storeToRefs } from 'pinia';
 import useCArmStore, { useCArmPhysicalParameters } from '../store/c-arm';
+
+const THICKNESS = 0.000001;
 
 export function useDetectorModel(view: View, imageID: MaybeRef<Maybe<string>>) {
   const { detectorDiameter } = storeToRefs(useCArmStore());
@@ -21,7 +23,7 @@ export function useDetectorModel(view: View, imageID: MaybeRef<Maybe<string>>) {
     const { detectorPos } = useCArmPosition(imageID);
     const size = detectorDiameter.value;
     source.setXLength(size);
-    source.setYLength(size);
+    source.setYLength(THICKNESS);
     source.setZLength(size);
     source.setCenter(...(detectorPos.value as Vector3));
     source.setRotations(armTilt.value, 0, armRotation.value);
