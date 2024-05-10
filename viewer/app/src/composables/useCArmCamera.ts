c22707c6aa45d134e5552f53b7859837a2b7161d
Forrest <forrest.li@kitware.com>
2024-05-10 11:47:02 -0400

    viewer: convert view angle with perspective view

--- viewer/app/src/composables/useCArmCamera.ts
+++ viewer/app/src/composables/useCArmCamera.ts
@@ -7,32 +7,40 @@ import { View } from '@/src/core/vtk/types';
 import { vtkFieldRef } from '@/src/core/vtk/vtkFieldRef';
 
 const RAD_TO_DEG = 180 / Math.PI;
+const VIEW_ANGLE_BORDER_FACTOR = 1.1;
 
 export function useCArmCamera(view: View, imageID: MaybeRef<Maybe<string>>) {
   const { detectorDiameter, sourceToDetectorDistance } = storeToRefs(
     useCArmStore()
   );
 
-  const { emitterPos, emitterDir, emitterUpDir } = useCArmPosition(imageID);
+  const { emitterPos, centerPos, emitterDir, emitterUpDir } =
+    useCArmPosition(imageID);
   const size = vtkFieldRef(view.renderWindowView, 'size');
 
   watchEffect(() => {
     const emitterViewAngle =
-      RAD_TO_DEG *
       2 *
       Math.atan2(detectorDiameter.value / 2, sourceToDetectorDistance.value);
 
     const [width, height] = size.value;
     const aspect = width / height;
     // Camera view angle is the vertical view angle
-    const cameraViewAngle =
-      width <= height ? emitterViewAngle / aspect : emitterViewAngle;
+    let cameraViewAngle = emitterViewAngle;
+    if (width < height) {
+      // To keep the entire detector in the view, treat emitterViewAngle as
+      // the horizontal camera view angle.
+      // similar triangles: width = aspect, height = 1
+      const x = aspect / 2 / Math.tan(emitterViewAngle / 2);
+      cameraViewAngle = 2 * Math.atan2(0.5, x);
+    }
 
     const cam = view.renderer.getActiveCamera();
     cam.setPosition(...emitterPos.value);
     cam.setDirectionOfProjection(...emitterDir.value);
     cam.setViewUp(...emitterUpDir.value);
-    cam.setViewAngle(cameraViewAngle);
+    cam.setFocalPoint(...centerPos.value);
+    cam.setViewAngle(cameraViewAngle * RAD_TO_DEG * VIEW_ANGLE_BORDER_FACTOR);
 
     view.renderer.resetCameraClippingRange();
     view.requestRender();
