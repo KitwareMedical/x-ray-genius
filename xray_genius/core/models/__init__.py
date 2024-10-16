9f0a9cc2ee23852bb1345e18f6c3ef3e1815f075
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-10-15 22:32:04 -0400

    Add DB models for sample data

--- xray_genius/core/models/__init__.py
+++ xray_genius/core/models/__init__.py
@@ -1,11 +1,14 @@
 from .ct_input_file import CTInputFile
 from .input_parameters import InputParameters
 from .output_image import OutputImage
+from .sample_dataset import SampleDataset, SampleDatasetFile
 from .session import Session
 
 __all__ = [
     'CTInputFile',
     'InputParameters',
     'OutputImage',
+    'SampleDataset',
+    'SampleDatasetFile',
     'Session',
 ]
---
+++ xray_genius/core/models/sample_dataset.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class SampleDataset(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    license = models.TextField()
+    source = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class SampleDatasetFile(models.Model):
+    sample_dataset = models.ForeignKey(
+        SampleDataset, on_delete=models.CASCADE, related_name='files'
+    )
+    file = models.FileField(upload_to='sample_data')
