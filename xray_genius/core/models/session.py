0e38a1f3717c1facd432193bd60a256a0caa9768
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-11-26 22:25:11 -0500

    Lost worker/stuck session detection

--- xray_genius/core/models/session.py
+++ xray_genius/core/models/session.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from datetime import timedelta
 import uuid
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 from django_extensions.db.fields import CreationDateTimeField
 
 from .ct_input_file import CTInputFile
@@ -13,6 +16,19 @@ class SessionManager(models.Manager):
     def get_queryset(self) -> models.QuerySet[Session]:
         # Hide sessions that are being deleted
         return super().get_queryset().exclude(status=Session.Status.DELETING)
+
+
+class StuckSessionsManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet[Session]:
+        # Get sessions that have been running or queued for too long
+        return (
+            super()
+            .get_queryset()
+            .filter(
+                status__in=[Session.Status.QUEUED, Session.Status.RUNNING],
+                created__lt=timezone.now() - timedelta(seconds=settings.SESSION_TIMEOUT),
+            )
+        )
 
 
 class Session(models.Model):
@@ -34,6 +50,7 @@ class Session(models.Model):
     output_images_zip = models.FileField(upload_to='output_images/zips', null=True, blank=True)
 
     objects = SessionManager()
+    stuck_objects = StuckSessionsManager()
 
     def __str__(self) -> str:
         return f'Session {self.id} ({self.status})'
