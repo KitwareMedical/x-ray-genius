1f83acad08cbe3b6161b906ffb030dbedeb7a8c6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-01-02 11:50:47 -0500

    Display Session duration in admin

--- xray_genius/core/admin/__init__.py
+++ xray_genius/core/admin/__init__.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.contrib import admin
+from django.db.models import Max
+import humanize
 
 from xray_genius.core.models import (
     ContactFormSubmission,
@@ -34,11 +38,27 @@ class SessionOutputImageInline(admin.TabularInline):
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
-    list_display = ('created', 'owner', 'status')
+    list_display = ('created', 'owner', 'status', 'get_duration')
     inlines = (
         SessionInputParametersInline,
         SessionOutputImageInline,
     )
+
+    def get_queryset(self, request):
+        # Prefetch the maximum created timestamp from related output_images
+        # so we have it for the get_duration method
+        qs = super().get_queryset(request)
+        return qs.annotate(latest_image_created=Max('output_images__created'))
+
+    @admin.display(description='Duration')
+    def get_duration(self, obj: Session) -> str:
+        # The duration of the Session can be calculated as the difference between the
+        # most recent OutputImage and the Session's creation time.
+        most_recent_image_created: datetime | None = getattr(obj, 'latest_image_created', None)
+        if most_recent_image_created:
+            duration: timedelta = most_recent_image_created - obj.created
+            return humanize.precisedelta(duration)
+        return 'N/A'
 
 
 class SampleDatasetFileInline(admin.TabularInline):
